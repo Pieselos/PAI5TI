0521ac794f0529a23052201d885b401dd703540a
Pieselos <koobav123@gmail.com>
2024-09-25 09:05:47 +0200

    jajo

---
+++ prakt1/organizer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sierpniowy kalendarz</title>
+    <link rel="stylesheet" href="./styl5.css">
+</head>
+<body>
+    <header>
+        <section id="headerBlok1">
+            <h1>Organizer: SIERPIEŃ</h1>
+        </section>
+        <section id="headerBlok2">
+            <form method="POST">
+                <label>Zapisz wydarzenie: <input name="wydarzenie"></label>
+                <input type="submit" value="ok">
+            </form>
+        </section>
+        <section id="headerBlok3">
+            <img src="pliki/logo2.png" alt="sierpień">
+        </section>
+        
+    </header>
+    <main>
+
+    </main>
+    <footer>
+         <p>Stronę wykonał: Jakub Wójtowicz</p>
+    </footer>
+</body>
+</html>
